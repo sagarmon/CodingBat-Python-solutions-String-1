a2c1c1726949835cf63bd9c9b19a36fa9ab4daa5
Sagar Sarkar <sagar_mon@hotmail.com>
2020-07-31 14:50:47 +0600

    Create without_end.py

---
+++ without_end.py
@@ -0,0 +1,8 @@
+"""
+Given a string, return a version without the first and last char, 
+so "Hello" yields "ell". The string length will be at least 2.
+"""
+
+def without_end(str):
+  if len(str) >= 2:
+    return str[1:(len(str) - 1)]
